e4e80ebccb88df80f3c5abf7c21da69993982f3d
Adam Leis <adam.m.leis@gmail.com>
2025-06-25 07:07:11 -0400

    update camera controls module to include ortho views

--- src/addCamera.ts
+++ src/addCamera.ts
@@ -12,31 +12,30 @@ interface Cameras {
 export function addCamera(canvas: HTMLCanvasElement, scene: Scene, gui: GUI) {
     const cameraOptions = ['perspective', 'top', 'side', 'front'] as const;
 
+    const size = 100;
     const cameras: Cameras = {
         perspective: new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000),
-        top: new OrthographicCamera(-1, -1, 10, -1),
-        side: new OrthographicCamera(-1, -1, 10, -1),
-        front: new OrthographicCamera(-1, -1, 10, -1),
+        top: new OrthographicCamera(-1 * size, size, size, -1 * size),
+        side: new OrthographicCamera(-1 * size, size, size, -1 * size),
+        front: new OrthographicCamera(-1 * size, size, size, -1 * size),
     };
 
-    // const cameras: Record<typeof cameraOptions[number], PerspectiveCamera | OrthographicCamera> = {};
+    cameras.top.position.set(0, size, 0);
+    cameras.top.lookAt(0, 0, 0);
+    cameras.side.position.set(size, 0, 0);
+    cameras.side.lookAt(0, 0, 0);
+    cameras.front.position.set(0, 0, size);
+    cameras.front.lookAt(0, 0, 0);
 
-    // cameras.perspective = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-    // cameras.perspective = perspective;
-    cameras.perspective.position.set(2, 2, 5);
+    cameras.perspective.position.set(-4, 5, -5);
 
     let activeCamera: PerspectiveCamera | OrthographicCamera = cameras.perspective;
-    const getActiveCamera = (): PerspectiveCamera | OrthographicCamera => {
-        // console.log('activeCamera', activeCamera);
-        return activeCamera
-    } ;
+    const getActiveCamera = (): PerspectiveCamera | OrthographicCamera => activeCamera;
 
-    const cameraControls = new OrbitControls(cameras.perspective, canvas);
+    let cameraControls = new OrbitControls(cameras.perspective, canvas);
     cameraControls.enableDamping = true;
     cameraControls.autoRotate = false;
     cameraControls.update();
-
-    // cameras.top = new OrthographicCamera(-1, -1, 10, -1);
 
     scene.add(cameras.perspective, cameras.top);
 
@@ -44,14 +43,31 @@ export function addCamera(canvas: HTMLCanvasElement, scene: Scene, gui: GUI) {
         options: cameraOptions,
         active: cameraOptions[0],
     };
-    const cameraFolder = gui.addFolder('Cameras');
-    cameraFolder.add(cameraDebug, 'active', cameraOptions).onChange(console.log);
+    const cameraFolder = gui.addFolder('🎥 Cameras');
+    cameraFolder.add(cameraDebug, 'active', cameraOptions)
+        .onChange((curCam: keyof Cameras) => {
+            activeCamera = cameras[curCam];
+            cameraControls.dispose();
+            cameraControls = new OrbitControls(activeCamera, canvas);
+            cameraControls.enableDamping = true;
+            cameraControls.autoRotate = false;
+            cameraControls.enableRotate = !(activeCamera as OrthographicCamera).isOrthographicCamera;
+            cameraControls.update();
+        });
 
     function resizeCamera() {
-        if (activeCamera === cameras.perspective) {
-            cameras.perspective.aspect = canvas.clientWidth / canvas.clientHeight;
-            cameras.perspective.updateProjectionMatrix();
-        } // maybe rules if ortho is focused and screen changes?
+        const { width, height } = canvas;
+        const aspect = width / height;
+        if ((activeCamera as OrthographicCamera).isOrthographicCamera) {
+            const orthoCam = activeCamera as OrthographicCamera;
+            orthoCam.left = -size * aspect;
+            orthoCam.right = size * aspect;
+            orthoCam.top = size;
+            orthoCam.bottom = -size;
+        } else {
+            (activeCamera as PerspectiveCamera).aspect = aspect;
+        }
+        activeCamera.updateProjectionMatrix();
     }
 
     return { cameras, cameraControls, getActiveCamera, resizeCamera };
